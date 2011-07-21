76fd273165207def04f3779473c9641b2dfee77d
Mansoor Sayed <mansoor@mansoorsayed.com>
2011-07-21 08:27:32 -0400

    tidy

--- filesystemdir.h
+++ filesystemdir.h
@@ -19,27 +19,49 @@ public slots:
     void jsReset();
 
     QString currentPath();
+
     void setCurrentPath(QString path);
+
     QString cleanPath(QString path);
+
     QString homePath();
+
     QString tempPath();
+
     bool isAbsolutePath(QString path);
+
     bool isRelativePath(QString path);
+
     bool match(QString exp, QString path);
+
     QString rootPath();
+
     QString separator();
+
     QString toNativeSeparators(QString path);
+
     bool isReadable(QString path);
+
     bool isRootPath(QString path);
+
     bool mkdir(QString name);
+
     bool mkpath(QString path);
+
     bool rename(QString oldName, QString newName);
+
     bool rmdir(QString name);
+
     bool rmpath(QString path);
+
     QString dirName(QString path);
+
     QString relativeFilePath(QString path);
+
     QString absoluteFilePath(QString filePath);
+
     QString absolutePath(QString path);
+
     bool exists(QString path);
 };
 
